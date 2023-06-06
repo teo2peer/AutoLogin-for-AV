14a11c821bd92508f32be0dc6f74db2a27ed821d
Teo2Peer <a@a.com>
2023-06-06 15:33:08 +0200

    Added content.css

--- js/popup.js
+++ js/popup.js
@@ -1,122 +1,3 @@
-// Helper functions
-
-// Thanks to EditThisCookie extension for the code and help understanding the Chrome Cookies API in depth
-function getUrlVars() {
-    var d = [], c;
-    var a = window.location.href.slice(window.location.href.indexOf("?") + 1).split("&");
-    for (var b = 0; b < a.length; b++) {
-        c = a[b].split("=");
-        d.push(c[0]);
-        d[c[0]] = c[1]
-    }
-    return d
-}
-
-function rawToSet(untransformed) {
-    var cookieToSet = {};
-    // if untransformed domain is an IP address or after :// have a dot, we need to remove the leading dot
-
-
-    if (untransformed.domain.substr(0, 1) == '.') {
-        untransformed.domain = untransformed.domain.substring(1);
-    }
-
-    untransformed.secure = true;
-    cookieToSet.url = "https://" + untransformed.domain + "/login/index.php";
-    cookieToSet.domain = untransformed.domain;
-
-    cookieToSet.name = untransformed.name;
-    cookieToSet.value = untransformed.value;
-
-    cookieToSet.path = untransformed.path;
-    cookieToSet.httpOnly = untransformed.httpOnly;
-    if (!untransformed.session) {
-        cookieToSet.expirationDate = untransformed.expirationDate;
-    }
-    cookieToSet.storeId = untransformed.storeId;
-    //cookieToSet.sameSite = "strict";
-    return cookieToSet;
-}
-
-function deleteCookie(url, name, store, callback) {
-    console.log("Borrando cookie " + name + " con url " + url + "...");
-    chrome.cookies.remove({
-        'url': "https://aulavirtual.uji.es",
-        'name': name,
-        'storeId': store
-    }, function (details) {
-        if (typeof callback === "undefined")
-            return;
-        if (details === "null" || details === undefined || details === "undefined") {
-            callback(false);
-        } else {
-            callback(true);
-        }
-    })
-}
-
-
-
-function addCookie(cookie) {
-    chrome.cookies.getAllCookieStores(function (ckSt) {
-        for (let x = 0; x < ckSt.length; x++) {
-            if (ckSt[x].tabIds.indexOf(currentTabID) != -1) {
-                cookie.storeId = ckSt[x].id;
-                break;
-            }
-        }
-
-
-        // expiration date 1 year from now
-        var expirationDate = new Date();
-        expirationDate.setFullYear(expirationDate.getFullYear() + 1);
-
-        deleteCookie(cookie.url, cookie.name, cookie.storeId, function () {
-            chrome.cookies.set(rawToSet(cookie));
-        });
-    });
-}
-
-function restoreCookies() {
-
-
-    chrome.storage.local.get(cookiesList, function (data) {
-
-        for (var i = 0; i < cookiesList.length; i++) {
-            var cookie = data[cookiesList[i]];
-            if (cookie) {
-                addCookie(cookie);
-            }
-        }
-    });
-}
-
-// Helepr variables
-var currentTabID;
-var currentTabURL;
-
-
-
-function start() {
-
-    var arguments = getUrlVars();
-    if (arguments.url === undefined) {
-        chrome.tabs.query(
-            {
-                active: true,
-                lastFocusedWindow: true
-            },
-            function (tabs) {
-                let currentTabURL = tabs[0].url;
-                currentTabID = tabs[0].id;
-            }
-        );
-    } else {
-        var url = decodeURI(arguments.url);
-        currentTabID = parseInt(decodeURI(arguments.id));
-        document.title = document.title + "-" + url;
-    }
-}
 
 
 
@@ -129,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var buttonMessage = document.getElementById("buttonMessage");
     var buttonStart = document.getElementById("start");
     var cookiesList = ["MoodleSessionaulavirtualuji", "MDL_SSP_AuthToken", "MDL_SSP_SessID"];
-    start();
 
 
 
@@ -179,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    buttonStart.addEventListener("click", restoreCookies);
+    // buttonStart.addEventListener("click", restoreCookies);
 
 
 
--- js/sw.js
+++ js/sw.js
@@ -1,13 +1,128 @@
+// Helper functions
+
+// Thanks to EditThisCookie extension for the code and help understanding the Chrome Cookies API in depth
+function buildUrl(domain, path, searchUrl) {
+    // Keep same protocol as searchUrl
+    // This fixes a bug when we want to unset 'secure' property in an https domain
+    var secure = searchUrl.indexOf("https://") === 0;
+
+    if (domain.substr(0, 1) === '.')
+        domain = domain.substring(1);
+
+    return "https://" + domain + path;
+}
+
+function rawToSet(untransformed) {
+    var cookieToSet = {};
+
+    untransformed.secure = true;
+    cookieToSet.url = buildUrl(untransformed.domain, untransformed.path, "https://" + untransformed.domain + "/login/index.php");
+
+
+    if (untransformed.domain.substr(0, 1) === '.')
+        untransformed.domain = untransformed.domain.substring(1);
+
+    // cookieToSet.domain = untransformed.domain;
+
+    cookieToSet.name = untransformed.name;
+    cookieToSet.value = untransformed.value;
+
+    cookieToSet.path = untransformed.path;
+    cookieToSet.httpOnly = untransformed.httpOnly;
+    if (!untransformed.session) {
+        cookieToSet.expirationDate = untransformed.expirationDate;
+    }
+    cookieToSet.storeId = untransformed.storeId;
+    //cookieToSet.sameSite = "strict";
+    return cookieToSet;
+}
+
+function deleteCookie(url, name, store, callback) {
+    console.log("Borrando cookie " + name + " con url " + url + "...");
+    chrome.cookies.remove({
+        'url': "https://aulavirtual.uji.es",
+        'name': name,
+        'storeId': store
+    }, function (details) {
+        if (typeof callback === "undefined")
+            return;
+        if (details === "null" || details === undefined || details === "undefined") {
+            callback(false);
+        } else {
+            callback(true);
+        }
+    })
+}
+
+
+
+function addCookie(cookie) {
+    chrome.cookies.getAllCookieStores(function (ckSt) {
+        for (let x = 0; x < ckSt.length; x++) {
+            if (ckSt[x].tabIds.indexOf(currentTabID) != -1) {
+                cookie.storeId = ckSt[x].id;
+                break;
+            }
+        }
+
+
+        // expiration date 1 year from now
+        var expirationDate = new Date();
+        expirationDate.setFullYear(expirationDate.getFullYear() + 1);
+
+        deleteCookie(cookie.url, cookie.name, cookie.storeId, function () {
+            chrome.cookies.set(rawToSet(cookie));
+        });
+    });
+}
+
+function restoreCookies() {
+
+
+    chrome.storage.local.get(cookiesList, function (data) {
+
+        for (var i = 0; i < cookiesList.length; i++) {
+            var cookie = data[cookiesList[i]];
+            if (cookie) {
+                addCookie(cookie);
+            }
+        }
+    });
+
+}
+
+// Helepr variables
+var currentTabID;
+var currentTabURL;
+var cookiesList = ["MoodleSessionaulavirtualuji", "MDL_SSP_AuthToken", "MDL_SSP_SessID"];
+
+
+
+
+function start() {
+
+
+    chrome.tabs.query(
+        {
+            active: true,
+            lastFocusedWindow: true
+        },
+        function (tabs) {
+            let currentTabURL = tabs[0].url;
+            currentTabID = tabs[0].id;
+        }
+    );
+
+}
+
+
+
+
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-
-
-
+    start();
 
     if (changeInfo && changeInfo.status === "complete" && tab.url && tab.url.startsWith("https://aulavirtual.uji.es/")) {
-
-
-
-
         var cookiesList = ["MoodleSessionaulavirtualuji", "MDL_SSP_AuthToken", "MDL_SSP_SessID"];
         for (var i = 0; i < cookiesList.length; i++) {
             chrome.cookies.get({
@@ -24,64 +139,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
             });
         }
-
-
-
-        if (tab.url === "https://aulavirtual.uji.es/login/index.php") {
-
-            chrome.storage.local.get("extensionEnabled", function (data) {
-                if (data.extensionEnabled != true) {
-                    return;
-                }
-
-
-                chrome.storage.local.get("loginAttempt", function (data) {
-
-                    // get local cookie if exist
-                    chrome.storage.local.get("copiedCookie", function (cookie) {
-                        if (!cookie.copiedCookie) {
-                            return;
-                        }
-                    });
-
-
-
-
-                    if (data.loginAttemptTimes) {
-                        var now = new Date();
-                        chrome.storage.local.get("loginAttemptTime", function () {
-                            if (now - data.loginAttemptTime > 300000) {
-                                chrome.storage.local.set({ "loginAttempt": 1 }, function () {
-                                    chrome.storage.local.set({ "loginAttemptTime": now }, function () {
-                                        // chrome.tabs.update(tabId, { url: "https://aulavirtual.uji.es/auth/saml2/login.php?wants=https%3A%2F%2Faulavirtual.uji.es%2Fmy%2Fcourses.php&passive=off" });
-                                    });
-                                }
-                                );
-                            } else {
-                                if (data.loginAttempt > 3) {
-                                    chrome.storage.local.remove("copiedCookie", function () {
-                                        console.log("Cookie borrada.");
-                                    });
-                                }
-                                chrome.storage.local.set({ "loginAttempt": data.loginAttempt + 1 }, function () {
-                                    chrome.storage.local.set({ "loginAttemptTime": now }, function () {
-                                        // chrome.tabs.update(tabId, { url: "https://aulavirtual.uji.es/auth/saml2/login.php?wants=https%3A%2F%2Faulavirtual.uji.es%2Fmy%2Fcourses.php&passive=off" });
-                                    });
-                                });
-                            }
-                        });
-                    } else {
-                        chrome.storage.local.set({ "loginAttempt": 1 }, function () {
-                            var now = new Date();
-                            chrome.storage.local.set({ "loginAttemptTime": now }, function () {
-                                // chrome.tabs.update(tabId, { url: "https://aulavirtual.uji.es/auth/saml2/login.php?wants=https%3A%2F%2Faulavirtual.uji.es%2Fmy%2Fcourses.php&passive=off" });
-                            });
-                        });
-                    }
-                });
-            });
-        }
-
 
     }
 });
@@ -101,8 +158,26 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             sendResponse(value);
 
         });
+        return true;
+    } else if (request.action === "restoreCookies") {
+        restoreCookies();
+        sendResponse({ "status": "success" });
 
-
+        return true;
+    } else if (request.action === "cookiesExist") {
+        // check if all cookies exist
+        var cookiesExist = true;
+        for (var i = 0; i < cookiesList.length; i++) {
+            chrome.cookies.get({
+                url: "https://aulavirtual.uji.es",
+                name: cookiesList[i]
+            }, function (cookie) {
+                if (!cookie) {
+                    cookiesExist = false;
+                }
+            });
+        }
+        sendResponse({ "status": cookiesExist });
         return true;
     }
 
