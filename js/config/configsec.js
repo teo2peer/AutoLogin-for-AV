ed803479db201a9588fb42ddce7ef92edc20a10b
Teo2Peer <teo2din@gmail.com>
2024-04-09 23:59:29 +0200

    There is an issue with microsoft antivirus when downloading from github

--- js/config/configsec.js
+++ js/config/configsec.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     // guardar en local storage
     chrome.storage.local.get("configurations", function (data) {
         var extensionConfig = data.configurations || [];
-        sc = extensionConfig.secret;
+        secret = extensionConfig.secret;
         username = extensionConfig.username;
 
         var qrcode = new QRCode("qrcode", {
-            text: "otpauth://totp/"+username+"?secret=" + sc + "&issuer=Universitat%20Jaume%20I",
+            text: "otpauth://totp/"+username+"?secret=" + secret + "&issuer=Universitat%20Jaume%20I",
             width: 256,
             height: 256,
             colorDark : "#000000",
@@ -15,7 +15,7 @@ $(document).ready(function () {
             correctLevel : QRCode.CorrectLevel.H
         });
 
-        $("#sc").val(sc);
+        $("#secret").val(secret);
     });
 
     $("#save").click(function () {
