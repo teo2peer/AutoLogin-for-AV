2916d55206392f0642d1ea3879766f3bff6b033e
Teo2Peer <teo2din@gmail.com>
2024-04-14 21:16:47 +0200

    Refactor popup.js and update configuration logic

--- js/popup.js
+++ js/popup.js
@@ -1,95 +1,50 @@
-
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
-
     var toggleSwitch = document.getElementById("toggleSwitch");
     var statusMessage = document.getElementById("statusMessage");
-
-
-
-
-
- 
-
-
-
 
     $("#config").click(function () {
         window.location.href = "/pages/config/config.html";
     });
 
-
-
-
-
     toggleSwitch.addEventListener("change", function () {
         chrome.storage.local.get("configurations", function (data) {
-            extensionConfig = data.configurations || [];
-
-
+            var extensionConfig = data.configurations || [];
             extensionConfig.extensionEnabled = toggleSwitch.checked;
             chrome.storage.local.set({ "configurations": extensionConfig });
-    
-            buttonMessage.textContent = (toggleSwitch.checked) ? "Activado" : "Desactivado";
-            statusMessage.textContent = statusMessageDisplay(extensionConfig.extensionEnabled, extensionConfig.configured );
+
+            buttonMessage.textContent = toggleSwitch.checked ? "Activado" : "Desactivado";
+            statusMessage.textContent = statusMessageDisplay(extensionConfig.extensionEnabled, extensionConfig.configured);
         });
     });
 
-    // clear configurations
-    // chrome.storage.local.clear();
-
     chrome.storage.local.get("configurations", function (data) {
-        var extensionConfig = data.configurations || [];
-    
-        if (!extensionConfig || extensionConfig.length == 0) {
-            extensionConfig = {
-                extensionEnabled: false, 
-                configured: false,
-                username: "",
-                page: 1,
-                password: "",
-                secret: "",
-            };
-            // Guardamos el array de configuraciones actualizado en el almacenamiento local
-            chrome.storage.local.set({ "configurations": extensionConfig });
-        }
+        var extensionConfig = data.configurations || {
+            extensionEnabled: false,
+            configured: false,
+            username: "",
+            page: 1,
+            password: "",
+            secret: "",
+        };
 
-        // Actualizamos el estado del toggle switch, el texto del botón y el mensaje de estado
+        chrome.storage.local.set({ "configurations": extensionConfig });
+
         toggleSwitch.checked = extensionConfig.extensionEnabled;
-        buttonMessage.textContent = (toggleSwitch.checked) ? "Activado" : "Desactivado";
+        buttonMessage.textContent = toggleSwitch.checked ? "Activado" : "Desactivado";
         statusMessage.textContent = statusMessageDisplay(extensionConfig.extensionEnabled, extensionConfig.configured);
-        
-        page = extensionConfig.page;
-        
-        
 
-        if(page == 1){
-            window.location.href = "/pages/setup/config1.html";
-        }else if(page == 2){
-            window.location.href = "/pages/setup/config2.html";
-        }else if(page == 3){
-            window.location.href = "/pages/setup/config3.html";
+        var page = extensionConfig.page;
+
+        if (page >= 1 && page <= 3) {
+            window.location.href = `/pages/setup/config${page}.html`;
         }
     });
-
-
 });
 
-function statusMessageDisplay(state, configured){
-    console.log(state + " " + configured);
-    if(state){
-        if(configured){
-            return "Activada, no tienes que volver a iniciar sesión en el Aula Virtual.";
-        }else{
-            return "Activada, pero debes configurar la extension por primera vez.";
-        }
-    }else{
+function statusMessageDisplay(state, configured) {
+    if (state) {
+        return configured ? "Activada, no tienes que volver a iniciar sesión en el Aula Virtual." : "Activada, pero debes configurar la extension por primera vez.";
+    } else {
         return "Desactivada, activa la extensión para no volver a tener que iniciar sesión.";
     }
-
 }
-
-
-
